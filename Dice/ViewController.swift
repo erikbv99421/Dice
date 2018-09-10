55b3fc25918c3ca139839b34859109db5d0cd16a
erik zephyr <erikbv99421@gmail.com>
2018-09-10 13:54:09 +0100

    Initial commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Erik Barreto de Vera on 10/9/18.
+//  Copyright © 2018 Erik Barreto de Vera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  var randomDiceIndex1: Int = 0
+  var randomDiceIndex2: Int = 0
+  
+  @IBOutlet weak var diceImageView1: UIImageView!
+  @IBOutlet weak var diceImageView2: UIImageView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func rollButtonPressed(_ sender: UIButton) {
+    randomDiceIndex1 = 1 + Int(arc4random_uniform(6))
+    randomDiceIndex2 = 1 + Int(arc4random_uniform(6))
+    
+  }
+  
+}
+
