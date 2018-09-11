392cab55ba06c07c6673aea96af9ad3e5485ec82
erik zephyr <erikbv99421@gmail.com>
2018-09-11 11:31:19 +0100

    mejorando codigo

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
   var randomDiceIndex1: Int = 0
   var randomDiceIndex2: Int = 0
+  let maxDiceNum: Int = 6
   
   @IBOutlet weak var diceImageView1: UIImageView!
   @IBOutlet weak var diceImageView2: UIImageView!
@@ -27,8 +28,8 @@ class ViewController: UIViewController {
   }
 
   @IBAction func rollButtonPressed(_ sender: UIButton) {
-    randomDiceIndex1 = 1 + Int(arc4random_uniform(6))
-    randomDiceIndex2 = 1 + Int(arc4random_uniform(6))
+    randomDiceIndex1 = 1 + Int(arc4random_uniform(UInt32(maxDiceNum)))
+    randomDiceIndex2 = 1 + Int(arc4random_uniform(UInt32(maxDiceNum)))
     
     print("Dice 1: \(randomDiceIndex1) \n Dice 2: \(randomDiceIndex2)")
     
