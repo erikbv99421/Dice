a4bbd315b11d52dce777afe168ca4bbd77f5d210
erik zephyr <erikbv99421@gmail.com>
2018-09-11 11:34:43 +0100

    optimizando codigo

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  var randomDiceIndex1: Int = 0
-  var randomDiceIndex2: Int = 0
-  let maxDiceNum: Int = 6
+  var randomDiceIndex : Int = 0
+  var randomDiceIndex2 : Int = 0
+  let maxDiceNum : Int = 6
+  let fixRandom : Int = 1
   
   @IBOutlet weak var diceImageView1: UIImageView!
   @IBOutlet weak var diceImageView2: UIImageView!
@@ -28,15 +29,14 @@ class ViewController: UIViewController {
   }
 
   @IBAction func rollButtonPressed(_ sender: UIButton) {
-    randomDiceIndex1 = 1 + Int(arc4random_uniform(UInt32(maxDiceNum)))
-    randomDiceIndex2 = 1 + Int(arc4random_uniform(UInt32(maxDiceNum)))
     
-    print("Dice 1: \(randomDiceIndex1) \n Dice 2: \(randomDiceIndex2)")
+    diceImageView1.image = UIImage(named: "dice\(randomIndex())")
+    diceImageView2.image = UIImage(named: "dice\(randomIndex())")
     
-    diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1)")
-    diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2)")
-    
-    
+  }
+  
+  func randomIndex() -> Int {
+    return fixRandom + Int(arc4random_uniform(UInt32(maxDiceNum)))
   }
   
 }
