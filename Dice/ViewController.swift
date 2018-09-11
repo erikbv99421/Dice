766df2115fd6c19131e4c2c5a4a72e47f1bb53e4
erik zephyr <erikbv99421@gmail.com>
2018-09-11 11:41:58 +0100

    random antes de empezar

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -20,12 +20,12 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    updateDiceImages()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    
   }
 
   @IBAction func rollButtonPressed(_ sender: UIButton) {
